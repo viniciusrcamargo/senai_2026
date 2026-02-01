5753e8d7a03f7418806ae6c5f8f4af8ae979106d
Instrutor <viniciusr.camargo@gmail.com>
2026-02-01 13:06:07 -0300

    Atividades aplicadas em sala de aula

--- PFE/3A/aula0902/exercicios/exercicio01.js
+++ PFE/3A/aula0902/exercicios/exercicio01.js
@@ -2,4 +2,4 @@ let pontuacao = 100;
 let divisaoPontuacao = pontuacao / 2;
 let resultado = divisaoPontuacao ** 2;
 
-console.log('O resultado da divisão, após potencia é ' + resultado);
+console.log('O resultado da divisão, após potencia é '+ resultado);
