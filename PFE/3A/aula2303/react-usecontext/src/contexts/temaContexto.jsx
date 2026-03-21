8204f6a6007314e078c4399b3ed3eaded31a09b5
Vinicius <viniciusr.camargo@gmail.com>
2026-03-21 12:49:37 -0300

    Aula sobre react useContext

---
+++ PFE/3A/aula2303/react-usecontext/src/contexts/temaContexto.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+
+export const ContextoTema = createContext(null);
+
+export function TemaProvedor({children}){
+    const [tema, setTema] = useState('light');
+
+    function mudarTema(){
+        //if ternário
+        setTema((prev) => (prev === 'light' ? 'dark' : 'light'))
+    }
+
+    const valor = {tema, mudarTema};
+
+    return(
+        <ContextoTema.Provider value={valor}>
+            {children}
+        </ContextoTema.Provider>
+    )
+}
+
