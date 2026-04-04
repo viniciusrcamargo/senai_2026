f7c59820c44f85198c039fec46519237eee9057e
Vinicius <viniciusr.camargo@gmail.com>
2026-04-04 10:53:10 -0300

    Inicio do projeto

--- PFE/3B/projeto/estacao-meteorologica-front/src/routes/index.jsx
+++ PFE/3B/projeto/estacao-meteorologica-front/src/routes/index.jsx
@@ -2,8 +2,8 @@ import {Routes, Route} from 'react-router-dom';
 
 //import das rotas
 import Login from '../pages/login';
-// import Registro from '../pages/registro';
-// import Dashboard from '../pages/dashboard';
+import Registro from '../pages/registro';
+import Dashboard from '../pages/dashboard';
 // import Cadastro from '../pages/cadastro';
 // import Relatorio from '../pages/relatorio';
 // import NotFound from '../pages/notfound';
@@ -12,6 +12,8 @@ export default function Rotas(){
     return(
         <Routes>
             <Route path='/' element={<Login />}/>
+            <Route path='/registro' element={<Registro />}/>
+            <Route path='/dashboard' element={<Dashboard />}/>
         </Routes>
     )
 }
