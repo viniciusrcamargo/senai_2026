1894a7a606495b3ba91f71d4462fa0694bfcbba6
Vinicius <viniciusr.camargo@gmail.com>
2026-04-23 08:17:43 -0300

    Finalizando a página de dashboard

--- PFE/3A/Projeto/estacao-meteorologica-front/src/pages/dashboard/index.jsx
+++ PFE/3A/Projeto/estacao-meteorologica-front/src/pages/dashboard/index.jsx
@@ -4,6 +4,65 @@ export default function Dashboard(){
     return(
         <>
         <Header />
+        <section className="cards">
+            <div className="card">
+                <h6>Temperatura</h6>
+                <h2>32º C</h2>
+            </div>
+            <div className="card">
+                <h6>Umidade</h6>
+                <h2>60%</h2>
+            </div>
+            <div className="card">
+                <h6>Pressão</h6>
+                <h2>1013 hPa</h2>
+            </div>
+            <div className="card">
+                <h6>Vento</h6>
+                <h2>12km</h2>
+            </div>
+        </section>
+
+        <section className="graficos">
+            <h6>Gráficos</h6>
+            <div>
+                {/* colocar gráficos */}
+            </div>
+        </section>
+
+        <section className="tabela">
+            <h6>Leituras Recentes</h6>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Horário</th>
+                        <th>Temp.</th>
+                        <th>Umidade</th>
+                        <th>Vento</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>12:00</td>
+                        <td>45ºC</td>
+                        <td>50%</td>
+                        <td>13km</td>
+                    </tr>
+                    <tr>
+                        <td>12:00</td>
+                        <td>45ºC</td>
+                        <td>50%</td>
+                        <td>13km</td>
+                    </tr>
+                    <tr>
+                        <td>12:00</td>
+                        <td>45ºC</td>
+                        <td>50%</td>
+                        <td>13km</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
         </>
     )
 }
