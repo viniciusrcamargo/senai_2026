aabb1d3aab1c21eee9c6f9af6d549bf5c3b76a16
Vinicius <viniciusr.camargo@gmail.com>
2026-05-27 13:14:32 -0300

    Ajustes nos estilos do app de navegação entre telas

--- PPDM/3A/21-05-26 - Navegacao entre telas/sesi-pet/app/pages/cadastro.jsx
+++ PPDM/3A/21-05-26 - Navegacao entre telas/sesi-pet/app/pages/cadastro.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+mport { useState } from "react";
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 
 export default function Cadastro({ navigation }) {
