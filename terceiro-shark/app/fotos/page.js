71cf1101faadfa5a53acaa552adb9fbf75d4d408
Vinicius <viniciusr.camargo@gmail.com>
2026-08-10 12:40:32 -0300

    Projeto em desenvolvimento com alunos

---
+++ terceiro-shark/app/fotos/page.js
@@ -0,0 +1,12 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+export default function Fotos(){
+    return(
+        <>
+            <Header />
+                <p>devo criar a página de fotos também =)</p>
+            <Footer />
+        </>
+    )
+}
