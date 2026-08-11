64260bcfca75b5cd7d7d26be4c9976fea7a5d0ea
Vinicius <viniciusr.camargo@gmail.com>
2026-08-11 12:41:33 -0300

    Projeto em desenvolvimento com alunos

--- PFE/3A/2 semestre/aula1308-subrotas/app/categorias/esportes/page.jsx
+++ PFE/3A/2 semestre/aula1308-subrotas/app/categorias/esportes/page.jsx
@@ -6,7 +6,7 @@ export default function Esportes() {
         <>
             <Header />
             <main>
-                <h2>Página de Esportes</h2>
+                <h2>Página de Esporte</h2>
             </main>
             <Footer />
         </>
