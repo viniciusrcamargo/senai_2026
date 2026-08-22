64f5dd4a2f773225cf89a2431631243f5fc58aef
Vinicius <viniciusr.camargo@gmail.com>
2026-08-22 12:56:08 -0300

    Projeto acesso api

--- PFE/3A/2 semestre/aula2408-acessoapi/app/buscaCep/page.jsx
+++ PFE/3A/2 semestre/aula2408-acessoapi/app/buscaCep/page.jsx
@@ -28,7 +28,7 @@ export default function BuscaCep(){
                 <div>
                     <p>Cep: {endereco.cep}</p>
                     <p>Rua: {endereco.logradouro}</p>
-                    <p>Cidade: {edereco.localidade}</p>
+                    <p>Cidade: {endereco.localidade}</p>
                     <p>UF: {endereco.uf}</p>
                 </div>
             )}
