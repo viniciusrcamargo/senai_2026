10d2c4bf9fc6ac797bb7ecd7c3119e2a352d60f9
Vinicius <viniciusr.camargo@gmail.com>
2026-08-30 13:07:17 -0300

    Next full stack

--- PFE/3A/2 semestre/aula3108-escola-fullstack/app/principal/page.jsx
+++ PFE/3A/2 semestre/aula3108-escola-fullstack/app/principal/page.jsx
@@ -9,7 +9,6 @@ export default function Principal(){
             <main>
                 <h2>Bem vindo ao Sistema Escolar - Sesi Mirandópolis</h2>
                 <p>Devo criar uma página inicial mais elaborada</p>
-                {/* <Image src={} /> utilizar imagem */}
             </main>
         </>
     )
