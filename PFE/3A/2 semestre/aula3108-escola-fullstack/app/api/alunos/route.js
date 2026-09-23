c5aef1c16a4aa35128b107ccaf6159f474e18115
Vinicius <viniciusr.camargo@gmail.com>
2026-09-23 12:21:12 -0300

    Continuação projeto escola

--- PFE/3A/2 semestre/aula3108-escola-fullstack/app/api/alunos/route.js
+++ PFE/3A/2 semestre/aula3108-escola-fullstack/app/api/alunos/route.js
@@ -19,12 +19,35 @@ export async function salvaAlunos(request){
     }
 }
 
-export async function editAlunos(request){
-
+export async function editAluno(request) {
+    try {
+        const dados = await request.json();
+        const sql = db.prepare(`
+            UPDATE alunos
+            SET nome = ?, idade = ?, serie = ?, ra = ?
+            WHERE id_aluno = ?
+        `);
+        sql.run(dados.nome, dados.idade, dados.serie, dados.ra, dados.id_aluno);
+        return NextResponse.json({
+            message: "Aluno atualizado com sucesso!"
+        });
+    } catch (error) {
+        console.error('Erro ao editar o aluno ', error);
+        return NextResponse.json({ message: "Erro ao editar o aluno." }, { status: 500 });
+    };
 }
 
-export async function excluiAlunos(request){
-
-} 
-
-
+// excluir aluno pelo id_aluno
+export async function excluiAluno(request) {
+    try {
+        const dados = await request.json();
+        const sql = db.prepare(`DELETE FROM alunos WHERE id_aluno = ?`);
+        sql.run(dados.id_aluno);
+        return NextResponse.json({
+            message: "Aluno excluído com sucesso!"
+        });
+    } catch (error) {
+        console.error('Erro ao excluir o aluno ', error);
+        return NextResponse.json({ message: "Erro ao excluir o aluno." }, { status: 500 });
+    };
+}
