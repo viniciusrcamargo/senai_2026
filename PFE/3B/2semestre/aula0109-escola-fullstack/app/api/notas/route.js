76d300a6dfc85a2b94b0344f10b201527a0706d7
Vinicius <viniciusr.camargo@gmail.com>
2026-09-25 09:28:44 -0300

    Criação da rota de notas

--- PFE/3B/2semestre/aula0109-escola-fullstack/app/api/notas/route.js
+++ PFE/3B/2semestre/aula0109-escola-fullstack/app/api/notas/route.js
@@ -2,25 +2,30 @@ import db from "../../db/banco";
 import { NextResponse } from "next/server";
 
 export async function GET(){
-    const alunos = db.prepare('SELECT * FROM alunos ORDER BY nome')
-    return NextResponse.json(alunos)
+    const notas = db.prepare(`SELECT alunos.nome, alunos.ra, notas.t1, notas.t2, notas.n1, notas.n2, notas.n3  FROM notas
+    INNER JOIN alunos
+    ON notas.aluno_id = alunos.id_aluno
+    ORDER BY alunos.nome`)
+    return NextResponse.json(notas)
 }
 
 export async function POST(request){
     try {
         const dados =  await request.json();
-        const sql = db.prepare(`INSERT INTO alunos (nome, idade, serie, ra) VALUES (?,?,?,?)`)
+        const sql = db.prepare(`INSERT INTO notas (aluno_id, t1, t2, n1, n2, n3) VALUES (?,?,?,?,?,?)`)
         sql.run(
-            dados.nome,
-            dados.idade,
-            dados.serie,
-            dados.ra
+            dados.aluno_id,
+            dados.t1,
+            dados.t2,
+            dados.n1,
+            dados.n2,
+            dados.n3
         )
         return NextResponse.json({
-            mensagem: 'Aluno salvo com sucesso!'
+            mensagem: 'Nota salva com sucesso!'
         });
     } catch (error) {
-        console.error('Erro ao salvar usuário ', error)
+        console.error('Erro ao salvar ', error)
     }
 }
 
